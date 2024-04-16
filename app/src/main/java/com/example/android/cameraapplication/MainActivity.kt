51ba5a5fc3fadc4c8e5f1aec047360a3b71b0ca4
loremazze <mazzeilorenzo35@virgilio.it>
2024-04-16 14:18:14 +0200

    Fixed Camera and first implementation of Landmark detection

--- app/src/main/java/com/example/android/cameraapplication/MainActivity.kt
+++ app/src/main/java/com/example/android/cameraapplication/MainActivity.kt
@@ -10,35 +10,56 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
 import android.Manifest
+import android.util.Log
 import android.widget.Button
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
 import androidx.camera.core.CameraProvider
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.google.mediapipe.examples.poselandmarker.PoseLandmarkerHelper
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),PoseLandmarkerHelper.LandmarkerListener {
 
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     companion object{
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
     }
 
+    lateinit var poseLandmarkerHelper: PoseLandmarkerHelper
     var cam: CameraSelector? = null
     var camProv: ProcessCameraProvider? = null
+    var camera: Camera? = null
     var prev: Preview? = null
+    var imageAnalyzer: ImageAnalysis? = null
+    lateinit var backgroundExecutor: ExecutorService /** Blocking ML operations are performed using this executor */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize our background executor
+        backgroundExecutor = Executors.newSingleThreadExecutor()
         requestCameraPermissions()
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
 
-            camProv = cameraProvider
+            camProv = cameraProvider //maybe redundant zio pera
 
             val previewView = findViewById<PreviewView>(R.id.previewView)
 
             val preview = androidx.camera.core.Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                //.setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
                 .build()
                 .also {
                     it.setSurfaceProvider(previewView.surfaceProvider)
@@ -48,6 +69,20 @@ class MainActivity : AppCompatActivity() {
 
             val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
             cam = cameraSelector
+            //bindCameraUseCases() //TODO tentativo di usare mediapipe
+
+            imageAnalyzer =
+                ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                    //.setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                    .build()
+                    // The analyzer can then be assigned to the instance
+                    .also {
+                        it.setAnalyzer(backgroundExecutor) { image ->
+                            detectPose(image)
+                        }
+                    }
 
             try {
                 cameraProvider.unbindAll()
@@ -56,6 +91,18 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }, ContextCompat.getMainExecutor(this))
+
+        backgroundExecutor.execute {
+            poseLandmarkerHelper = PoseLandmarkerHelper(
+                context = this,
+                runningMode = RunningMode.LIVE_STREAM,
+                minPoseDetectionConfidence = PoseLandmarkerHelper.DEFAULT_POSE_DETECTION_CONFIDENCE,
+                minPoseTrackingConfidence = PoseLandmarkerHelper.DEFAULT_POSE_TRACKING_CONFIDENCE,
+                minPosePresenceConfidence = PoseLandmarkerHelper.DEFAULT_POSE_PRESENCE_CONFIDENCE,
+                currentDelegate = PoseLandmarkerHelper.DELEGATE_CPU,
+                poseLandmarkerHelperListener = this
+            )
+        }
 
         findViewById<Button>(R.id.button2).setOnClickListener{
             if(cam == CameraSelector.DEFAULT_FRONT_CAMERA)
@@ -71,6 +118,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /*
+    private fun bindCameraUseCases() {
+
+        // CameraProvider
+        val cameraProvider = camProv
+            ?: throw IllegalStateException("Camera initialization failed.")
+
+        val cameraSelector =
+            CameraSelector.DEFAULT_FRONT_CAMERA
+
+
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        /*preview = Preview.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+            .build()
+        */
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, prev, imageAnalyzer
+            )
+
+            // Attach the viewfinder's surface provider to preview use case
+            //prev?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            //Log.e(CameraFragment.TAG, "Use case binding failed", exc)
+        }
+    }*/
+
+    private fun detectPose(imageProxy: ImageProxy) {
+        if(this::poseLandmarkerHelper.isInitialized) {
+            poseLandmarkerHelper.detectLiveStream(
+                imageProxy = imageProxy,
+                isFrontCamera = true
+            )
+        }
+    }
+
     private fun requestCameraPermissions() {
         if (ContextCompat.checkSelfPermission(
                 this,
@@ -83,6 +175,19 @@ class MainActivity : AppCompatActivity() {
                 CAMERA_PERMISSION_REQUEST_CODE
             )
         }
+    }
+
+    override fun onError(error: String, errorCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onResults(
+        resultBundle: PoseLandmarkerHelper.ResultBundle
+    ) {
+        println(resultBundle.results)
+        println(resultBundle.inputImageHeight)
+        println(resultBundle.inputImageWidth)
+
     }
 
     override fun onRequestPermissionsResult(
