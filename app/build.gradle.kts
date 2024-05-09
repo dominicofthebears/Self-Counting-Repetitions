46cf5de65918aa9938f9b9dddece23804f977691
dominicofthebears <domenicodorsi01@gmail.com>
2024-05-09 13:08:22 +0200

    Added end exercise report

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ val ktorVersion: String by project
 
 dependencies {
 
+
+    implementation("androidx.fragment:fragment-ktx:1.7.0")
     implementation("io.ktor:ktor-server-netty:1.4.0")
     implementation("io.ktor:ktor:1.4.0")
     implementation("io.ktor:ktor-gson:1.4.0")
